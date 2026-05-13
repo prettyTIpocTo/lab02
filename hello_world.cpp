1abbbf1ff1a84a536a7614a714ce7688cbbaaf33
prettyTIpocTo <sashaysh1356@mail.ru>
2026-05-13 19:08:42 +0300

    formatted with clang-format Mozilla style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-using namespace std;
 int main(){
     string name;
-    cin >> name;
-    cout << "Hello world from @" << name << endl;
+    std::cin >> name;
+    std::cout << "Hello world from @" << name << std::endl;
 }
